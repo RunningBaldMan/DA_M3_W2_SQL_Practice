eef439a274f8032456e3490b5f99be7849385ed2
Justin Masters <justinmasters@Mac.attlocal.net>
2025-10-12 10:59:04 -0400

    Saving progress

--- AssignmentFiles/SQL_joins.sql
+++ AssignmentFiles/SQL_joins.sql
@@ -5,15 +5,52 @@ USE coffeeshop_db;
 -- =========================================================
 
 -- Q1) Join products to categories: list product_name, category_name, price.
+SELECT products.name AS product_name,
+categories.name AS category_name,
+products.price
+FROM products 
+INNER JOIN categories
+ON products.category_id = categories.category_id;
 
 -- Q2) For each order item, show: order_id, order_datetime, store_name,
 --     product_name, quantity, line_total (= quantity * products.price).
 --     Sort by order_datetime, then order_id.
+SELECT order_items.order_id,
+	order_items.quantity,
+	orders.order_datetime,
+	orders.store_id,
+	stores.name AS store_name,
+	products.name AS product_name,
+	order_items.quantity * products.price AS line_total
+FROM order_items
+INNER JOIN orders ON order_items.order_id = orders.order_id
+INNER JOIN stores ON stores.store_id = orders.store_id
+INNER JOIN products ON order_items.product_id = products.product_id
+ORDER BY
+	order_datetime,
+	order_id;
 
 -- Q3) Customer order history (PAID only):
 --     For each order, show customer_name, store_name, order_datetime,
 --     order_total (= SUM(quantity * products.price) per order).
-
+SELECT 
+	CONCAT(customers.first_name,' ', customers.last_name) AS customer_name,
+	stores.name AS store_name,
+    orders.order_datetime,
+    SUM(order_items.quantity * products.price) AS order_total
+FROM
+	orders
+INNER JOIN customers ON orders.customer_id = customers.customer_id
+INNER JOIN stores ON orders.store_id = stores.store_id
+INNER JOIN order_items ON orders.order_id = order_items.order_id
+INNER JOIN products ON order_items.product_id = products.product_id
+Where
+	orders.status = 'paid'
+GROUP BY
+	orders.order_id, customer_name, store_name, orders.order_datetime
+ORDER BY 
+	orders.order_datetime;
+    
 -- Q4) Left join to find customers who have never placed an order.
 --     Return first_name, last_name, city, state.
 
